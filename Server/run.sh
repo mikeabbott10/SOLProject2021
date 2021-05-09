a4ce71b2db57f4e3b9acf1a124ea2e6e07ef047b
mikeabbott10 <amorelli.lorenzo@gmail.com>
2021-05-09 18:39:56 +0200

    minor changes

--- Server/run.sh
+++
@@ -1,2 +0,0 @@
-cd /mnt/dati/Uni/anno2.2/SistemiOperativi/Laboratorio/Progetto/Progetto2021/Server
-./bin/server
--- Server/run_with_valgrind.sh
+++
@@ -1,2 +0,0 @@
-cd /mnt/dati/Uni/anno2.2/SistemiOperativi/Laboratorio/Progetto/Progetto2021/Server
-valgrind --leak-check=full ./bin/server
